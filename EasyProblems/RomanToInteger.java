6e0504fe49b2cfae23b0acba261f8b200707db3c
Taouil Hanane <73299910+hanane54@users.noreply.github.com>
2024-12-15 13:39:03 +0100

    Roman To Integer problem

---
+++ EasyProblems/RomanToInteger.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000);
+
+        int resultInt = 0;
+        int stringLength = s.length();
+        if ((stringLength >= 1) && (15 >= stringLength)) {
+            for (int i = 0; i < stringLength; i++) {
+                int current = romanMap.get(s.charAt(i));
+                if (i < stringLength-1 && (romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1)))) {
+                    resultInt -= current;
+                } else {
+                    resultInt += current;
+                }
+
+            }
+
+        }
+        return resultInt;
+    }
+}
