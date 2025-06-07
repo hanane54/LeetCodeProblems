617e8577b247bef3a9ee33cf866cbf1c56d6814a
TAOUIL Hanane <hanane59taouil@gmail.com>
2025-06-08 00:14:09 +0100

    Initialize the validator

---
+++ AmazonInterview/validation-api/src/main/java/com/company/validation/api/Main.java
@@ -0,0 +1,17 @@
+package com.company.validation.api;
+
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Create validator and configure rules
+        // 2. Add rules for attributes
+        // 3. Test inputs
+        
+
+        // 4. Validate them
+       
+        // 5.Print errors to the user
+    }
+} 
