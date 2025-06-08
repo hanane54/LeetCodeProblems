4edc73edf768e1dceea80aa4a4837a90da760747
TAOUIL Hanane <hanane59taouil@gmail.com>
2025-06-08 20:07:13 +0100

    Check if two Trees are the same

---
+++ EasyProblems/SameTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ( p == null && q == null ){
+            return true; 
+        }
+        if ( p != null && q != null && p.left == null && q.left == null && p.right ==null && q.right ==null  ){
+            return p.val == q.val ; 
+        }
+        if ( p == null || q == null ){
+            return false; 
+        }
+        if ( p.left == null && q.left ==null && p.right !=null && q.right !=null ){
+            return  isSameTree(p.right, q.right) ; 
+        }
+        if ( p.left != null && q.left !=null && p.right ==null && q.right ==null ){
+            return isSameTree(p.left, q.left)  ; 
+        }
+
+        if ( p.left != null && q.left !=null && p.right !=null && q.right !=null ){
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right) ; 
+        }
+        return false; 
+    }
+}
