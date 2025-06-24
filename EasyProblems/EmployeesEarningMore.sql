74751714806dffa93b3f84f897dbec48225368ae
Taouil Hanane <73299910+hanane54@users.noreply.github.com>
2025-06-24 14:56:21 +0100

    Update doc for problem n 181

--- EasyProblems/EmployeesEarningMore.sql
+++ EasyProblems/EmployeesEarningMore.sql
@@ -1,3 +1,5 @@
+-- can be found on https://leetcode.com/problems/employees-earning-more-than-their-managers/submissions/1674961168/
+-- Runtime 388ms Beats 51.92%
 
 SELECT name as Employee FROM 
     (SELECT a.name, b.name as manager_name
