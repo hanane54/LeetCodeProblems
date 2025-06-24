dad94a6dfc3b89a1e69a03a543b225245b9abae7
Taouil Hanane <73299910+hanane54@users.noreply.github.com>
2025-06-24 15:34:01 +0100

    Update sql Duplicate emails

--- EasyProblems/Emails.sql
+++ EasyProblems/Emails.sql
@@ -1,2 +1,4 @@
+-- can be found in https://leetcode.com/problems/duplicate-emails/
+-- Runtime 421ms Beats 19.45
 -- simple practice for sql: get duplicate emails 
 select email as Email from Person group by email having count(email)>1;
