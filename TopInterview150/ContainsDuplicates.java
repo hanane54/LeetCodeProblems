b159fd2e9c8e71b871eea36cd773c4145848f111
TAOUIL Hanane <hanane59taouil@gmail.com>
2025-08-02 20:06:59 +0100

    Improve memory usage for ContainsDuplicates Algorithm

--- TopInterview150/ContainsDuplicates.java
+++ TopInterview150/ContainsDuplicates.java
@@ -1,13 +1,33 @@
+package TopInterview150;
+
+// Updated to improve memory usage 
+
+import java.util.HashSet;
+import java.util.Set;
+
 class ContainsDuplicates {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         // // abs(0 - 3) <= 3
-        for ( int i=0; i<nums.length; i++ ){
-            for ( int j=i+1; j<nums.length; j++ ){
-                if ( nums[i] == nums[j] && Math.abs(j-i) <= k  ){
-                    return true;
-                }
+        // for ( int i=0; i<nums.length; i++ ){
+        //     for ( int j=i+1; j<nums.length; j++ ){
+        //         if ( nums[i] == nums[j] && Math.abs(j-i) <= k  ){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
             }
         }
+        
         return false;
     }
 }
