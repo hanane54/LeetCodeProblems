c4d264b9d64956add6e9618664a0950c0a3f7ddb
Taouil Hanane <73299910+hanane54@users.noreply.github.com>
2025-08-07 10:11:43 +0100

    Search insert position in a sorted list

---
+++ TopInterview150/SearchInsertPosition.java
@@ -0,0 +1,12 @@
+class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int i=0;
+        while ( i<nums.length ){
+            if( nums[i] >= target ){
+                return i;
+            } 
+            i++;
+        }
+        return i;
+    }
+}
