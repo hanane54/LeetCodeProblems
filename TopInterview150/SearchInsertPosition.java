c1bacbf8bc86488fc76bef56da2cd1275e309818
Taouil Hanane <73299910+hanane54@users.noreply.github.com>
2025-08-07 10:12:55 +0100

    Update Search insert Position, complexity O(log n)

--- TopInterview150/SearchInsertPosition.java
+++ TopInterview150/SearchInsertPosition.java
@@ -1,3 +1,6 @@
+// Runtime 0ms Beats 100.00% 
+// Memory 43.12MB Beats 38.59%
+// can be found on https://leetcode.com/problems/search-insert-position/submissions/1726657393/
 class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int i=0;
